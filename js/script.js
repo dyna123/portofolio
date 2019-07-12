961df195b23a0d16887bb220e49df552abd7e7d4
idham <idhamkholid965@gmail.com>
2019-07-12 14:33:23 +0700

    pembuatan parallax

--- js/script.js
+++ js/script.js
@@ -18,3 +18,36 @@ $('.page-scroll').on('click', function(e) {
    var headroom  = new Headroom(myElement);
    // initialise
    headroom.init(); 
+
+//parallax
+//about
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+
+    //jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/8 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+
+    // portofolio
+    if( wScroll > $('.portofolio').offset().top - 250 ) {
+       $('.gambar').each(function(i){
+            setTimeout(function(){
+                 $('.gambar').eq(i).addClass('muncul');
+            }, 600 * (i+1));
+       });
+    }
+});
