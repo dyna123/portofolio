3649743a5ceefa00021656e4048d92d8c4d19635
idham <idhamkholid965@gmail.com>
2019-07-12 14:40:55 +0700

    mengubah jquery

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ $(window).on('load', function(){
     $('.pKiri').addClass('pMuncul');
     $('.pKanan').addClass('pMuncul');
 });
+
 $(window).scroll(function() {
     var wScroll = $(this).scrollTop();
 
